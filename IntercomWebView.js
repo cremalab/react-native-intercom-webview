44c9dd02a3a413feae0ab64a3035b801f0a299be
Scotty Moon <scotty@scottymoon.com>
2017-10-03 13:28:58 -0500

    updating proptype bits and packages

--- IntercomWebView.js
+++ IntercomWebView.js
@@ -1,23 +1,24 @@
-import React, {Component } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { View, WebView, Dimensions } from 'react-native';
 import Spinner from 'react-native-loading-spinner-overlay';
 
-class IntercomWebView extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            isLoading: true
-        };
-    }
+class IntercomWebView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true
+    };
+  }
 
-    componentDidMount = () => {
-        this.setState({
-            windowHeight: Dimensions.get('window').height
-        });
-    }
+  componentDidMount = () => {
+    this.setState({
+      windowHeight: Dimensions.get('window').height
+    });
+  }
 
-    injectedJS = (appId, name, email, hideLauncher) => {
-        return `
+  injectedJS = (appId, name, email, hideLauncher) => {
+    return `
             window.Intercom('boot', {
                 app_id: '${appId}',
                 name: '${name}',
@@ -27,48 +28,48 @@ class IntercomWebView extends Component{
             
             if (${hideLauncher})
                 window.Intercom('showMessages');`;
-    }
+  }
 
-    onLoadEnd = () => {
-        this.setState({isLoading: false});
+  onLoadEnd = () => {
+    this.setState({ isLoading: false });
 
-        if (this.props.onLoadEnd)
-            this.props.onLoadEnd();
-    }
+    if (this.props.onLoadEnd)
+      this.props.onLoadEnd();
+  }
 
-    render(){
-        const { appId, name, email, hideLauncher, defaultHeight, showLoadingOverlay, ...remainingProps } = this.props;
-        const { isLoading, windowHeight } = this.state;
+  render() {
+    const { appId, name, email, hideLauncher, defaultHeight, showLoadingOverlay, ...remainingProps } = this.props;
+    const { isLoading, windowHeight } = this.state;
 
-        let height = defaultHeight || windowHeight;
+    let height = defaultHeight || windowHeight;
 
-        return(
+    return (
 
-            <View style={[{height: height}, this.props.style]}>
-                <Spinner visible={showLoadingOverlay && isLoading} />
-                <WebView source={require('./IntercomWebView.html')}
-                         injectedJavaScript={this.injectedJS( appId, name, email, hideLauncher )}
-                         javaScriptEnabled={true}
-                         onLoadEnd={this.onLoadEnd}
-                        {...remainingProps}
-                />
-            </View>
-        )
-    }
+      <View style={[{ height: height }, this.props.style]}>
+        <Spinner visible={showLoadingOverlay && isLoading} />
+        <WebView source={require('./IntercomWebView.html')}
+          injectedJavaScript={this.injectedJS(appId, name, email, hideLauncher)}
+          javaScriptEnabled={true}
+          onLoadEnd={this.onLoadEnd}
+          {...remainingProps}
+        />
+      </View>
+    )
+  }
 }
 
 IntercomWebView.PropTypes = {
-    appId: React.PropTypes.string,
-    name: React.PropTypes.string,
-    email: React.PropTypes.string,
-    hideLauncher: React.PropTypes.bool,
-    showLoadingOverlay: React.PropTypes.bool,
-    defaultHeight: React.PropTypes.number
+  appId: PropTypes.string,
+  name: PropTypes.string,
+  email: PropTypes.string,
+  hideLauncher: PropTypes.bool,
+  showLoadingOverlay: PropTypes.bool,
+  defaultHeight: PropTypes.number
 };
 
 IntercomWebView.defaultProps = {
-    hideLauncher: false,
-    showLoadingOverlay: true
+  hideLauncher: false,
+  showLoadingOverlay: true
 };
 
 export default IntercomWebView;
